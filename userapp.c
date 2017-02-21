2969f0fd37a2c995b85505add3bf0d9d01ddf281
sdatla2 <sdatla2@illinois.edu>
2017-02-20 23:08:12 -0600

    Debugging proc read #5

--- userapp.c
+++ userapp.c
@@ -1,34 +1,38 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 
-int factorial(int n){
+void factorial(int n){
   int temp=0, result=1;
   for(temp=1;temp<=n; temp++){
-      result=result*temp;
+    result=result*temp;
   }
-  print("Result: %d\n", result);
+  printf("Result: %d\n", result);
 }
 
 int main(int argc, int *argv[])
-{	long pid;
-	int err;
-	pid=(long)getpid();
-	FILE *proc_entry_fs; 
-	proc_entry_fs = fopen("/proc/mp1/status", "w");
-	if(proc_entry_fs==NULL){
-		printf("failed to open file \n");
-	}
+{
 
+  long pid;
+  int err;
+  pid=(long)getpid();
+  FILE *proc_entry_fs;
+  proc_entry_fs = fopen("/proc/mp1/status", "w");
+  if(proc_entry_fs==NULL){
+    printf("failed to open file \n");
+  }
+  char str[256];
+  sprintf(str, "%ld", pid);
 
-char str[256];
-sprintf(str, "%ld", pid);
+  err = fprintf(proc_entry_fs, "%s",str);
+  printf("pid=%s err=%d \n",str,err);
+  fclose(proc_entry_fs);
 
-	err=fprintf (proc_entry_fs, "%s",str);
-	printf("pid=%s err=%d \n",str,err);
-	fclose(proc_entry_fs);
-
-	return 0;
+  int i = 0;
+  for(i = 1; i <= 20; i++){
+    factorial(i);
+  }
+  return 0;
 }
