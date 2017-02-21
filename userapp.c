8a2aaf17ba042fda3e9a41c1a9807bed931f11bc
zli135 <zli135@illinois.edu>
2017-02-20 21:51:10 -0600

    Update userapp.c

--- userapp.c
+++ userapp.c
@@ -1,6 +1,27 @@
-#include "userapp.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
 
-int main(int argc, char* argv[])
-{
+
+int main(int argc, int *argv[])
+{	long pid;
+	int err;
+	pid=(long)getpid();
+	FILE *proc_entry_fs; 
+	proc_entry_fs = fopen("/proc/mp1/status", "w");
+	if(proc_entry_fs==NULL){
+		printf("failed to open file \n");
+	}
+
+
+char str[256];
+sprintf(str, "%ld", pid);
+
+	err=fprintf (proc_entry_fs, "%s",str);
+	printf("pid=%s err=%d \n",str,err);
+	fclose(proc_entry_fs);
+
 	return 0;
 }
