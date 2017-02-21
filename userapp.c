2b3039bd2e22b935e02591d0450edd4403146cc4
zli135 <zli135@illinois.edu>
2017-02-21 02:12:51 -0600

    Update userapp.c

--- userapp.c
+++ userapp.c
@@ -1,11 +1,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <string.h>
 
 void factorial(int n){
-  int temp=1, result=1;
+  int temp=0, result=1;
   for(temp=1;temp<=n; temp++){
     result *= temp;
   }
@@ -13,14 +13,24 @@ void factorial(int n){
 }
 
 int main(int argc, int *argv[])
-{
-  unsigned long pid = getpid();
-  char str[256];
-  sprintf(str, "echo '%lu'>/proc/mp1/status", pid);
-  system(str);
-  int i = 0;
-  for(i = 1; i <= 20; i++){
-    factorial(i);
-  }
-  return 0;
+{	long pid;
+	int err;
+	pid=(long)getpid();
+	FILE *proc_entry_fs; 
+	proc_entry_fs = fopen("/proc/mp1_z/status", "w");
+	if(proc_entry_fs==NULL){
+		printf("failed to open file \n");
+	}
+
+	char str[256];
+	sprintf(str, "%ld", pid);
+
+	err=fprintf (proc_entry_fs, "%s",str);
+	printf("pid=%s err=%d \n",str,err);
+	fclose(proc_entry_fs);
+
+	factorial(200);
+	// int i=0;
+	// for(i=0; i<1000000000;i++){}     //loop used to extend running time so that get_cpu_time() can get a non-zero cpu time.
+	return 0;
 }
