10ab3b1070799c6f8dae5497d27cdb5fa82fbd10
sdatla2 <sdatla2@illinois.edu>
2017-02-21 00:10:56 -0600

    Rewriting Proc Read

--- userapp.c
+++ userapp.c
@@ -7,7 +7,7 @@
 void factorial(int n){
   int temp=1, result=1;
   for(temp=1;temp<=n; temp++){
-    result=result*temp;
+    result *= temp;
   }
   printf("Result: %d\n", result);
 }
@@ -15,25 +15,13 @@ void factorial(int n){
 int main(int argc, int *argv[])
 {
 
-  long pid;
-  int err;
-  pid=(long)getpid();
-  FILE *proc_entry_fs;
-  proc_entry_fs = fopen("/proc/mp1/status", "w");
-  if(proc_entry_fs==NULL){
-    printf("failed to open file \n");
-  }
+  unsigned long pid = getpid();
   char str[256];
-  sprintf(str, "%ld", pid);
-
-  err = fprintf(proc_entry_fs, "%s",str);
-  printf("pid=%s err=%d \n",str,err);
-  fclose(proc_entry_fs);
-
+  sprintf(str, "echo '%lu'>/proc/mp1/status", pid);
+  system(str);
   int i = 0;
-  for(i = 1; i <= 2000; i++){
+  for(i = 1; i <= 20; i++){
     factorial(i);
-    sleep(100);
   }
   return 0;
 }
